c82c7b600eb6c53621a23fe7760a77a5cbf8cca6
kaiky <kaikycintra@gmail.com>
2026-04-07 02:35:06 -0300

    feat: reads tasks for scheduler

--- src/ep1.c
+++ src/ep1.c
@@ -2,13 +2,60 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define MAX_TASK_NUM 50
 
 // --------------- LÓGICA DE ENTRADA ---------------
-// nome deadline t0 dt
-// nome é string com 32 caracteres alocados
-// deadline é instante de tempo em que gostaríamos que o processo acabe, >=t0+dt
-// t0 é quando o processo chega no sistema
-// dt é o tempo de processamento
+typedef struct {
+    char name[32];
+    double t0;
+    double deadline;
+    double dt;
+
+    // dados a serem salvos durante execução
+    double time_left;
+    double tf;
+    bool active;
+    bool is_finished;
+} Task;
+
+Task* tasks_from_trace(char* filename, int* num_tasks) {
+    FILE *fp = fopen(filename, "r");
+    int count = 0;
+    Task* queue = malloc(MAX_TASK_NUM * sizeof(Task));
+
+    // cada linha tem: nome deadline t0 dt
+    // nome é string com 32 caracteres alocados
+    // deadline é instante de tempo em que gostaríamos que o processo acabe, >=t0+dt
+    // t0 é quando o processo chega no sistema
+    // dt é o tempo de processamento
+    while(fscanf(fp, "%31s %lf %lf %lf", 
+            queue[count].name, 
+            &queue[count].deadline, 
+            &queue[count].t0, 
+            &queue[count].dt) == 4) {
+        
+        queue[count].time_left = queue[count].dt;
+        queue[count].tf = 0;
+        queue[count].active = false;
+        queue[count].is_finished = false;
+
+        count++;
+    }
+
+    fclose(fp);
+    *num_tasks = count;
+    return queue;
+}
+
+void show_tasks(Task* tasks, int num_tasks) {
+    for(int i=0; i < num_tasks; i++) {
+            printf("task: %s | deadline: %.1f | t0: %.1f | dt: %.1f\n", 
+                    tasks[i].name, tasks[i].deadline, tasks[i].t0, tasks[i].dt);
+        }
+}
 
 
 
@@ -32,9 +79,12 @@
 // arg2, arquivo trace de entrada
 // arg3, arquivo a ser criado para saída
 int main(int argc, char **argv) {
-    printf("%s\n", argv[1]);
-    printf("%s\n", argv[2]);
-    printf("%s\n", argv[3]);
+    int num_tasks = 0;
+    Task* tasks = tasks_from_trace(argv[2], &num_tasks);
 
-    read_trace_file();
+    if (tasks != NULL) {
+        show_tasks(tasks, num_tasks);
+        free(tasks);
+    }
+    return 0;
 }
